877f4f4d6667ebe08d5238e304c1cab9c80bd46f
cortes-web-tech <cortes.web.tech@gmail.com>
2023-05-16 21:44:09 -0400

    Created login page. Hid db_connection info

--- todo.php
+++ todo.php
@@ -2,35 +2,24 @@
  <head><title>SQL Query</title></head>
   <body>
 
-  <?php
-  $dbHost = 'localhost';
-  $dbUser = 'dbUser2';
-  $password = 'kappapassword';
-  $database = 'inSession';
-  $table = 'todo';
-  $conn = mysqli_connect($dbhost, $dbUser, $password, $database);
-
-  if(mysqli_connect_errno()) {
-   echo "failed to connect to MySQL" . mysqli_connect_error();
-    exit();
-  }
-  echo 'successfully connected to MySQL.' . "</br></br/>";
+<?php
+include "db_conn.php";
+ 
 
 $sql = "SELECT * FROM todo;";
  $results = mysqli_query($conn, $sql);
  $resultCheck = mysqli_num_rows($results);
 
  if($resultCheck > 0){
- echo "to do list</br>";
+ echo "to do list</br></br>";
    while($row = mysqli_fetch_assoc($results)){
       echo $row['content'] . "  " . $row['completed'] . "</br>";
    }
 }
-mysql_close('$conn');
-?>
 
-<?php
 
+
+// mysql_close('$conn');
 ?>
 
 </body>
