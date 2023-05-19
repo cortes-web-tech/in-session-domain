1eeac48f15c43fce352a473c637745cb2e1cbc67
cortes-web-tech <cortes.web.tech@gmail.com>
2023-05-18 21:30:53 -0400

    Additional Date/Time cleanup

--- session.php
+++ session.php
@@ -4,6 +4,17 @@ $sql = "SELECT * FROM sessionData;";
  $results = mysqli_query($conn, $sql);
  $resultCheck = mysqli_num_rows($results);
 
+  function getTime($dateT){
+    $tmpTime = strtotime($dateT);
+    $dateT = date("h:i a", $tmpTime);
+    return $dateT;
+  }
+
+  function getDay($dateC){
+    $tmpDate = strtotime($dateC);
+    $dateC = date("l, m/d", $tmpDate);
+    return $dateC;
+  }
 
  if($resultCheck > 0){
   // echo "Data successfully retrieved from database.</br></br>";
@@ -21,7 +32,9 @@ $sql = "SELECT * FROM sessionData;";
  			<tr>
  				<td><h4>Session Title</h4></td>
  				<td><h4>Session Room</h4> </td>
+        <td><h4>Day</h4></td>
  				<td><h4>Start Time</h4></td>
+        <td><h4>End Time</h4></td>
 				<td><h4>Moderators(s)</h4></td>
  			</tr>
  			<tr>
@@ -30,11 +43,10 @@ $sql = "SELECT * FROM sessionData;";
     ?>
       <td><?php echo $row['title'];?></td>
       <td><?php echo $row['room'];?></td>
-      <td><?php $date = $row['startTime'];
-		$tmpDate = strtotime($date);
-		$date = date("l, m/d h:i a", $tmpDate);
-		echo $date;?></td>
-	<td><?php echo $row['modName'];?></td>
+      <td><?php echo getDay($row['startTime']);?></td>
+      <td><?php echo getTime($row['startTime']);?></td>
+      <td><?php echo getTime($row['endTime']);?></td>
+      <td><?php echo $row['modName'];?></td>
       </tr>
     <?php
    }
