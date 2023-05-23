a548f267604e5bc827a87cbd1562d68b8c080388
cortes-web-tech <cortes.web.dev@gmail.com>
2023-05-23 00:13:24 +0000

    bugfix

--- homepage_presenter.php
+++ homepage_presenter.php
@@ -1,12 +1,12 @@
 <?php
 include "db_conn.php";
-$tmpName = $_SESSION['user_name'];
-$sql = "SELECT * FROM subsessionData WHERE user_name='$tmpName';";
-$sql2 = "SELECT * FROM sessionData WHERE user_name='$tmpName;";
- $results = mysqli_query($conn, $sql);
- $resultCheck = mysqli_num_rows($results);
+$tmpName = $_SESSION['name'];
+$sql =  "SELECT * FROM subsessionData WHERE presenter='$tmpName';";
 
-  function getTime($dateT){
+$results = mysqli_query($conn, $sql);
+$checkResults = mysqli_num_rows($results);
+
+function getTime($dateT){
     $tmpTime = strtotime($dateT);
     $dateT = date("h:i a", $tmpTime);
     return $dateT;
@@ -18,45 +18,42 @@ $sql2 = "SELECT * FROM sessionData WHERE user_name='$tmpName;";
     return $dateC;
   }
 
- if($resultCheck > 0){
-  // echo "Data successfully retrieved from database.</br></br>";
- ?>
+
+?>
+
  <!DOCTYPE html>
  <html>
- 	<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
- 		<title>Session Data</title>
- 	</head>
- 	<body>
+        <head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+    	</head>
 
-    <h1>Displaying data for presenters only.</h1>
-		<div class="session_data_wrapper">
- 		<table class="session_data_table">
- 			<tr>
- 				<td><h4>Subsession Title</h4></td>
- 				<td><h4>Presenter</h4> </td>
-        <td><h4>Day</h4></td>
- 				<td><h4>Start Time</h4></td>
-        <td><h4>End Time</h4></td>
-				<td><h4>Moderators(s)</h4></td>
- 			</tr>
- 			<tr>
- 				<?php
-   while($row = mysqli_fetch_assoc($results)){
-    ?>
-      <td><a href=""><?php echo $row['subsession_title'];?></a></td>
-      <td><?php echo $row['user_name'];?></td>
-      <td><?php echo getDay($row['startTime']);?></td>
-      <td><?php echo getTime($row['startTime']);?></td>
-      <td><?php echo getTime($row['endTime']);?></td>
-      <td><?php echo $row['modName'];?></td>
-      </tr>
-    <?php
-   }
-}
- 				?>
+	<body>
+		<h1>Displaying data for presenters only.</h1>
+                <div class="session_data_wrapper">
+                <table class="session_data_table">
+			<tr>
+                                <td><h4>Subsession Title</h4></td>
+                                <td><h4>Presenter</h4> </td>
+        			<td><h4>Day</h4></td>
+                                <td><h4>Start Time</h4></td>
+			        <td><h4>End Time</h4></td>
+                                <td><h4>Moderators(s)</h4></td>
+                        </tr>
+			<tr>
+			 <?php while ($row = mysqli_fetch_assoc($results)){
+			?>
+				<td><a href=""><?php echo $row['subsession_title'];?></td>
+				<td><?php echo $row['presenter'];?></td>
+				<td><?php echo getDay($row['startTime']);?></td>
+				<td><?php echo getTime($row['startTime']);?></td>
+				<td><?php echo getTime($row['endTime']);?></td>
+				<td><?php echo $row['modName'];?></td>
+			</tr>
+			<?php
+			}
+			?>
+		</table>
+		</div>
+	</body>
 
- 		</table>
-</div>
- 	</body>
- </html>
+</html>
