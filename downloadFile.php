e0802124818a9b455fbad032e5ba4fcb2a11b5cd
cortes-web-tech <cortes.web.dev@gmail.com>
2023-05-23 15:18:37 +0000

    Download file bugfix

--- downloadFile.php
+++ downloadFile.php
@@ -1,23 +1,27 @@
-	<?php
-		session_start();
-		include "db_conn";
+<?php
+//	session_start();
+	include "db_conn";
+//	echo "Landed in download page. </br>";
 
-		echo "Landed in download page. </br>";
-		if(!empty($_GET['file']))
-		{
-			$filename = basename($_GET['file']);
-			$filepath = './uploads/' . $filename;
-			echo "File path: " . $filepath;
-			if(!empty($filename) && file_exists($filepath)){
-				echo "</br>File exists.";
+	if(!empty($_GET['file']))
+	{
+		$filename = basename($_GET['file']);
+		$filepath = 'uploads/' . $filename;
+		echo "File path: " . $filepath;
+		if(!empty($filename) && file_exists($filepath)){
+			echo "</br>File: " . $filename;
+			echo "</br> Filepath: " . $filepath;
 				// Define headers
 				header("Cache-Control: public");
 				header("Content-Description: File Transfer");
-				header("Content-Disposition: Attachment; Filename=$filename");
-//				header("Content-Type: application/zip");
-//				header("Content-Transfer-Encoding: Binary");
-			//	readfile($filepath);
-				exit();
+				header("Content-Disposition: attachment; filename=" . basename($_GET['file']));
+				header('Content-Type: application/force-download');
+				header("Content-Transfer-Encoding: Binary");
+				header('Expires: 0');
+				header('Pragma: public');
+//				header('Content-Length:' . filesize('uploads/' . $filename);
+				@readfile('uploads/' . basename($_GET['file']));
+				exit;
 			}
 			else{
 				echo "</br>This file does not exist.";
@@ -25,7 +29,7 @@
 			}
 		}
 
-	//	header("Location: index.php");
-	//		exit();
-	?>
+//	header("Location: index.php");
+//		exit();
+?>
 
