c117a2bb61c0f9aa882db1451462c6cc97fc28a5
cortes-web-tech <cortes.web.tech@gmail.com>
2023-07-20 23:03:02 -0400

    V0.0.41 began adding layout to client app

--- client/inSession.go
+++ client/inSession.go
@@ -8,30 +8,34 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+
+	// "fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	win := a.NewWindow("inSession")
-	// win.Resize(fyne.NewSize(2560, 1600))
+
 	fmt.Printf("Starting inSession..\n\n")
-	// blue := color.NRGBA{R: 27, G: 122, B: 221, A: 255}
-	// text1 := canvas.NewText("Welcome to inSession", blue)
-	closeapp := widget.NewButton("close", func() {
+
+	closeapp := widget.NewButton("X", func() {
 		a.Quit()
 		fmt.Println("App closed successfully.")
 	})
+	blue := color.NRGBA{R: 27, G: 122, B: 221, A: 255}
+	red := color.NRGBA{R: 222, G: 22, B: 21, A: 255}
+	closeapp.ExtendBaseWidget(closeapp)
+	inSession := canvas.NewText("inSession", blue)
+	sessionTitle := canvas.NewText("Session Title", color.White)
+	sessionData := canvas.NewText("SessionData", color.White)
 
-	title := canvas.NewText("inSession", color.White)
-	// button := canvas.NewText("Close", color.White)
-	// button := canvas.New
-	top := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), title, layout.NewSpacer(), closeapp)
+	closeAppButton := container.NewMax(canvas.NewRectangle(red), closeapp)
+	top := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), inSession, layout.NewSpacer(), closeAppButton)
+	wrapper := container.NewMax(canvas.NewRectangle(blue), sessionTitle)
+	sessionDataWrapper := container.NewCenter(canvas.NewRectangle(blue), sessionData)
+	middle := container.NewMax(canvas.NewRectangle(blue), wrapper, layout.NewSpacer(), sessionDataWrapper)
 
-	// top.Move(fyne.NewPos(800, 0))
-	// top := canvas.NewText("inSession", color.White)
-	// left := canvas.NewText("left", color.White)
-	middle := canvas.NewRectangle(color.NRGBA{R: 27, G: 122, B: 221, A: 255})
 	content := container.NewBorder(top, nil, nil, nil, middle)
 
 	win.SetContent(content)
