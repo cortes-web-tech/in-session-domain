a9220b691bab5bbc1a8ba82d0f3142cbc40ad974
cortes-web-tech <cortes.web.tech@gmail.com>
2023-07-21 21:06:34 -0400

    v0.0.41 client UI changes

--- client/inSession.go
+++ client/inSession.go
@@ -18,29 +18,29 @@ func main() {
 	win := a.NewWindow("inSession")
 	blue := color.NRGBA{R: 27, G: 122, B: 221, A: 255}
 	lightblue := color.NRGBA{R: 58, G: 158, B: 255, A: 255}
-	green := color.NRGBA{R: 17, G: 182, B: 121, A: 255}
-	// red := color.NRGBA{R: 170, G: 18, B: 12, A: 255}
+	darkblue := color.NRGBA{R: 22, G: 56, B: 175, A: 255}
+	// green := color.NRGBA{R: 17, G: 182, B: 121, A: 255}
 
 	fmt.Printf("Starting inSession..\n\n")
 
 	closeapp := widget.NewButtonWithIcon("", theme.CancelIcon(), func() {
 		a.Quit()
-		fmt.Println("App closed successfully.")
 	})
 
 	appTitle := canvas.NewText("inSession", lightblue)
 	sessionTitle := container.NewCenter(canvas.NewText("Session Title", color.White))
 	sessionData := container.NewCenter(canvas.NewText("Session Data", color.White))
 
-	tableData := container.NewMax(canvas.NewRectangle(blue), container.NewCenter(canvas.NewText("data tablesss", color.White)))
+	footer := container.New(layout.NewPaddedLayout(), container.NewMax(canvas.NewRectangle(color.Transparent), container.NewCenter(canvas.NewText("Footer", color.White))))
 
 	top := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), appTitle, layout.NewSpacer(), closeapp)
 	topWrapper := container.NewMax(canvas.NewRectangle(color.Transparent), top)
-	wrapper := container.NewMax(canvas.NewRectangle(blue), sessionTitle)
-	sessionDataWrapper := container.NewMax(canvas.NewRectangle(blue), sessionData)
-	box3 := container.NewMax(canvas.NewRectangle(green), container.NewCenter(canvas.NewText("box 3", color.White)))
-	middle := container.New(layout.NewGridLayoutWithRows(4), wrapper, sessionDataWrapper, box3, tableData)
-	middleWrapper := container.NewMax(canvas.NewRectangle(lightblue), middle)
+	titleWrapper := container.New(layout.NewPaddedLayout(), container.NewMax(canvas.NewRectangle(blue), sessionTitle))
+	vspace := container.New(layout.NewVBoxLayout(), layout.NewSpacer(), canvas.NewText("", color.White), layout.NewSpacer())
+
+	sessionDataWrapper := container.New(layout.NewPaddedLayout(), container.NewMax(canvas.NewRectangle(blue), sessionData))
+	middle := container.New(layout.NewVBoxLayout(), vspace, titleWrapper, sessionDataWrapper, layout.NewSpacer(), footer)
+	middleWrapper := container.New(layout.NewMaxLayout(), container.NewMax(canvas.NewVerticalGradient(lightblue, darkblue), middle))
 
 	content := container.NewBorder(topWrapper, nil, nil, nil, middleWrapper)
 	win.SetContent(content)
