3be47b5a3fd4d5f0f61dae58a0eeec52d8ea1362
cortes-web-tech <cortes.web.tech@gmail.com>
2023-07-23 23:22:16 -0400

    v0.0.42 Began adding multi-page functionality to GUI

--- client/inSession.go
+++ client/inSession.go
@@ -30,14 +30,33 @@ func openFileOnClick() error {
 	cmd := exec.Command("open", filepath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	// cmd.Std
 	return cmd.Run()
+}
 
+func createMainWindow(app fyne.App) fyne.Window {
+	myWindow := app.NewWindow("Main Window")
+
+	// Add your content for the main window here
+
+	return myWindow
+}
+
+func createSettingsWindow(app fyne.App) fyne.Window {
+	myWindow := app.NewWindow("Settings Window")
+
+	// Add your content for the settings window here
+	myWindow.SetContent(container.NewMax(canvas.NewRectangle(color.NRGBA{R: 27, G: 122, B: 221, A: 255})))
+
+	return myWindow
 }
 
 func main() {
 	a := app.New()
-	win := a.NewWindow("inSession")
+	// win := a.NewWindow("inSession")
+	win := createMainWindow(a)
+	settingsWindow := createSettingsWindow(a)
+
+	win.FixedSize()
 	blue := color.NRGBA{R: 27, G: 122, B: 221, A: 255}
 	lightblue := color.NRGBA{R: 58, G: 158, B: 255, A: 255}
 	darkblue := color.NRGBA{R: 22, G: 56, B: 175, A: 255}
@@ -153,15 +172,35 @@ func main() {
 			canvas.NewRectangle(color.Transparent), container.NewCenter(dataTable), layout.NewSpacer()))
 	currentTime := container.NewCenter(canvas.NewText(time.Now().Format(time.Kitchen), blue))
 
+	// Help info window
+
+	helpWindow := container.New(layout.NewGridLayout(1), canvas.NewVerticalGradient(lightblue, darkblue))
+	top := container.New(layout.NewGridLayout(7),
+		container.NewCenter(widget.NewButtonWithIcon("", theme.HomeIcon(), func() {
+			// win.MainMenu()
+			// win.Canvas()
+		})),
+		layout.NewSpacer(), currentTime,
+		layout.NewSpacer(), appTitle,
+		layout.NewSpacer(), closeapp)
+	topWrapper := container.NewMax(canvas.NewRectangle(color.Transparent), top)
+	// win.MainMenu()
 	iconInfo := container.New(layout.NewGridLayout(4),
 		container.NewCenter(canvas.NewText("Refresh", color.White)),
 		container.NewCenter(canvas.NewText("Files", color.White)),
 		container.NewCenter(canvas.NewText("Help", color.White)),
 		container.NewCenter(canvas.NewText("How-to", color.White)),
 		container.NewCenter(widget.NewButtonWithIcon("", theme.FileIcon(), func() {})),
-		container.NewCenter(widget.NewButtonWithIcon("", theme.ComputerIcon(), func() {})),
+		container.NewCenter(widget.NewButtonWithIcon("", theme.ComputerIcon(), func() {
+			// win.setContent()
+			// win.Hide()
+			settingsWindow.Show()
+		})),
 		container.NewCenter(widget.NewButtonWithIcon("", theme.AccountIcon(), func() {})),
-		container.NewCenter(widget.NewButtonWithIcon("", theme.HelpIcon(), func() {})),
+		container.NewCenter(widget.NewButtonWithIcon("", theme.HelpIcon(), func() {
+			win.SetContent(container.NewBorder(topWrapper, nil, nil, nil, helpWindow))
+
+		})),
 	)
 	dayChoose := container.New(layout.NewGridLayout(3),
 		layout.NewSpacer(), layout.NewSpacer(), layout.NewSpacer(),
@@ -172,12 +211,11 @@ func main() {
 	chooseDay := container.NewMax(canvas.NewRectangle(color.Transparent), container.New(layout.NewGridLayout(1), canvas.NewRectangle(color.Transparent), container.New(layout.NewGridLayout(1), dayChoose)))
 	helpInfo := container.NewMax(canvas.NewRectangle(color.Transparent), container.New(layout.NewGridLayout(1), layout.NewSpacer(), iconInfo))
 	footer := container.New(layout.NewGridLayoutWithColumns(3), chooseDay, layout.NewSpacer(), helpInfo)
-	top := container.New(layout.NewGridLayout(5), currentTime, layout.NewSpacer(), appTitle, layout.NewSpacer(), closeapp)
-	topWrapper := container.NewMax(canvas.NewRectangle(color.Transparent), top)
 	middle := container.New(layout.NewGridLayoutWithRows(3), sessionInfoWrapper, dataContainer, footer)
 	middleWrapper := container.New(layout.NewMaxLayout(), container.NewMax(canvas.NewVerticalGradient(lightblue, darkblue), middle))
 
 	content := container.NewBorder(topWrapper, nil, nil, nil, middleWrapper)
+
 	win.SetContent(content)
 	win.SetFullScreen(true)
 	win.ShowAndRun()
